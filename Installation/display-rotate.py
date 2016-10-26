1c465b0eb15bea245fcc6a68d1cdd9e09752ba24
ignasiplaza <plaza.ignasi@gmail.com>
2016-10-26 09:33:04 +0200

    Added comments, xinput installer and 0 degrees option

--- Installation/display-rotate.py
+++ Installation/display-rotate.py
@@ -1,32 +1,19 @@
 #!/bin/python3
 
 #this script will rotate your display and touch panel
-#must have xinput installed
 #tested on Raspbian Jessie with Pixel on 10/23/2016 on a Raspberry Pi 3
 #with the Raspberry Pi Touch Display and python 3.4.2
 
-
+from subprocess import STDOUT, check_call
 import os
 
-def make_executable(path):
+def make_executable(path): #This function adds execution permissions to a file
     mode = os.stat(path).st_mode
     mode |= (mode & 0o444) >> 2
     os.chmod(path, mode)
 
-def rotate_touch(position):
-    xinput_script = open("/usr/local/bin/touch-rotate.sh", "w")
-    xinput_script.write("#!/bin/bash\n")
-    xinput_script.write("xinput set-prop 'FT5406 memory based driver' 'Evdev Axes Swap' 1\n")
-    xinput_script.write("xinput --set-prop 'FT5406 memory based driver' 'Evdev Axis Inversion' " + position + "\n")
-    xinput_script.close()
-    delete_words(["@/usr/local/bin/touch-rotate.sh"],"/home/pi/.config/lxsession/LXDE-pi/autostart" )
-    autostart = open("/home/pi/.config/lxsession/LXDE-pi/autostart", "a")
-    autostart.write("@/usr/local/bin/touch-rotate.sh")
-    autostart.close()
-    make_executable("/usr/local/bin/touch-rotate.sh")
-
-def delete_words(words, path):#words is a list of possible strings you want to substract
-    document = open(path, "r")
+def delete_words(words, path): #this functions deletes all file lines containig a word from the list words
+    document = open(path, "r")     
     lines = document.readlines()
     document.close()
     document = open(path, "w")
@@ -49,19 +36,37 @@ def delete_words(words, path):#words is a list of possible strings you want to s
             
     document.close()
 
+def rotate_touch(position): #This function rotates the touch panel coordinates
+    xinput_script = open("/usr/local/bin/touch-rotate.sh", "w") #first it writes an script with the commands
+    xinput_script.write("#!/bin/bash\n")
+    xinput_script.write("xinput set-prop 'FT5406 memory based driver' 'Evdev Axes Swap' 1\n")
+    xinput_script.write("xinput --set-prop 'FT5406 memory based driver' 'Evdev Axis Inversion' " + position + "\n")
+    xinput_script.close()
+    delete_words(["@/usr/local/bin/touch-rotate.sh"],"/home/pi/.config/lxsession/LXDE-pi/autostart" ) #it deletes a possible old code line with same information
+    autostart = open("/home/pi/.config/lxsession/LXDE-pi/autostart", "a")
+    autostart.write("@/usr/local/bin/touch-rotate.sh") #add the route to the script
+    autostart.close()
+    make_executable("/usr/local/bin/touch-rotate.sh") #make it executable
+
 
 print("You are going to rotate the screen and the touch panel 90º, 180º or 270º")
 
-str = ""
+check_call(['apt-get', 'install', '-y', 'xinput'],stdout=open(os.devnull,'wb'),stderr=STDOUT)
+
+str = "" 
 words = ["lcd_rotate=2","display_rotate=0","display_rotate=1","display_rotate=2","display_rotate=3","display_rotate=0x10000","display_rotate=0x20000"]
 
-while str not in ["90","180","270"]: 
-    str = input("Please Write 90, 180 or 270: ")
+while str not in ["0","90","180","270"]: #asks the user to select the angle of rotation
+    str = input("Please Write 0, 90, 180 or 270: ")
 
-delete_words(words,"/boot/config.txt")
+delete_words(words,"/boot/config.txt") #deletes old code lines to avoid colisons
 document = open("/boot/config.txt", "a")
 
-if str == "90":
+if str == "0":
+    document.write("\nlcd_rotate=0")
+    delete_words(["@/usr/local/bin/touch-rotate.sh"],"/home/pi/.config/lxsession/LXDE-pi/autostart" )
+
+elif str == "90":
     document.write("\ndisplay_rotate=1")
     rotate_touch("0 1")
     
